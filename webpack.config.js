63909ec01c916b3ff033cfee7bcfddcbb762ce11
Ikuo Degawa <degawa@ikuwow.com>
2019-08-24 23:21:34 +0900

    Fix indent of webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // default
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-        // dist/main.js
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
+  entry: './src/index.js', // default
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+    // dist/main.js
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
 
 };
