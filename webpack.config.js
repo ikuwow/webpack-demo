c3cdcbe1a9bfb43bcc2ea2765b50707ab0e28f3d
Ikuo Degawa <degawa@ikuwow.com>
2019-08-25 23:48:38 +0900

    Add optimization

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,10 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
   }
 };
