09102f5e7f0e99ffe852face9216feef900bec38
Ikuo Degawa <degawa@ikuwow.com>
2019-08-25 16:55:30 +0900

    Start Output Management: Create multiple bundle

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.addEventListener('click', printMe);
+
+  element.appendChild(btn);
 
   return element;
 }
