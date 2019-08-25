cec744e6df87499c8f0b1e3a19a8d1b80567a59b
Ikuo Degawa <degawa@ikuwow.com>
2019-08-25 14:28:55 +0900

    Import image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import _ from 'lodash';
 import './style.css';
+import yattemiyou from './yattemiyou.jpg';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const icon = new Image();
+  icon.src = yattemiyou;
+  element.appendChild(icon);
 
   return element;
 }
