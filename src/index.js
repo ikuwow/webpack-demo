9aac1d0358cb77364c8f9c7cfaf139b21d426582
Ikuo Degawa <degawa@ikuwow.com>
2019-08-25 14:51:39 +0900

    Clean Up 👋
    This is the end of section "Asset Management"

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import yattemiyou from './yattemiyou.jpg';
-import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
-
-  const icon = new Image();
-  icon.src = yattemiyou;
-  element.appendChild(icon);
-
-  console.log(Data);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -6,30 +6,5 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
     // dist/main.js
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      },
-      {
-        test: /\.csv$/,
-        use: ['csv-loader']
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
-      }
-    ]
   }
-
 };
