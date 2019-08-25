60edebe58a59aebd983d0886e94cd7b86605c3dc
Ikuo Degawa <degawa@ikuwow.com>
2019-08-25 17:36:24 +0900

    Start section Development: Set development mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // see: https://github.com/johnagan/clean-webpack-plugin/pull/138
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
