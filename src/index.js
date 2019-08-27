142547b9545d05167c46f984b4681b59dc79990d
Ikuo Degawa <degawa@ikuwow.com>
2019-08-27 22:39:49 +0900

    Use async and await

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-function getComponent() {
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    return element;
-  }).catch(error => 'An error occured while loading the component'); // eslint-disable-line no-unused-vars
+async function getComponent() {
+  const element = document.createElement('div');
+  const { default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
 }
 
 getComponent().then(component => {
