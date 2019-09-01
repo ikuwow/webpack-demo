1d7a26bc55d05170c3042189efbdfed89f30b960
Ikuo Degawa <degawa@ikuwow.com>
2019-09-01 18:25:01 +0900

    Add lazy loading

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import { cube } from './math.js';
 
 function component() {
   const element = document.createElement('div');
+  const button = document.createElement('button');
+  const br = document.createElement('br');
+
   element.innerHTML = _.join([
     'Hello', 'webpack!', '5 cubed is equal to ', cube(5)
   ], ' ');
+  button.innerHTML = 'Click me and look at the console.';
+  element.appendChild(br);
+  element.appendChild(button);
+
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    const print = module.default;
+    print();
+  });
+
   return element;
 }
 
--- src/print.js
+++ src/print.js
@@ -1,3 +1,5 @@
+console.log('The print.js module has loaded!');
+
 export default function printMe() {
   console.log('I get called from print.js!');
 }
