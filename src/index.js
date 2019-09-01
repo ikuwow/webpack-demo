55ba64c58d2d07c2cc682187455ed01836c3df5d
Ikuo Degawa <degawa@ikuwow.com>
2019-09-01 12:31:09 +0900

    Use math.cube
    now square() is dead code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import { cube } from './math.js';
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack!'], ' ');
+  element.innerHTML = _.join([
+    'Hello', 'webpack!', '5 cubed is equal to ', cube(5)
+  ], ' ');
   return element;
 }
 
